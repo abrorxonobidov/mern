8b87da61de566abf1d26509697858c42ce83f31b
abrorxonobidov <abrorkhon.obidov@gmail.com>
2022-02-08 20:24:00 +0500

    main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import Navbar from "./components/Navbar";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <div className="App">
-      Hello React
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="offset-lg-2 col-lg-8">
+            <Main />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ client/src/components/TravelBook.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TravelBook = () => {
+  return (
+    <div className="card mb-3 mt-3">
+        <img src="https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900" className="card-img-top" alt="..." />
+        <div classNameName="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
+    );
+};
+
+export default TravelBook;
