172a860c7ebac6c85c8f70ed6194baf9f3ce26e6
abrorxonobidov <abrorkhon.obidov@gmail.com>
2022-02-08 21:46:32 +0500

    update

--- client/src/components/TravelBook.js
+++ client/src/components/TravelBook.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+import {Link} from "react-router-dom";
 
 const TravelBook = () => {
+
+    const [travelBook, setTravelBook] = useState([])
+
+    const fetchData = async () => {
+        const {data} = await axios.get('http://localhost:5000/api/travel')
+        setTravelBook(data.travels)
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
-        <div className="card mb-3">
-            <img
-                src="https://media.cntraveler.com/photos/5cf96a9dd9fb41f17ed08435/master/pass/Eiffel%20Tower_GettyImages-1005348968.jpg"
-                className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                </p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-            </div>
+        <div>
+            {travelBook.map(tb => (
+                    <div key={tb.id} className="card mb-3">
+                        <img
+                            src={tb.image}
+                            className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">{tb.title}</h5>
+                            <p className="card-text">
+                                {tb.description}
+                            </p>
+                            <div className="d-flex justify-content-start">
+                                <Link to={'/update/' + tb._id} className="btn btn-primary me-2">
+                                    Update
+                                </Link>
+                                <form>
+                                    <button type="submit" className="btn btn-danger">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                )
+            )}
+
+
         </div>
     );
 };
